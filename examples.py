844d62abc48dd07c709cccd2ccfa3b7af45815e2
adam <ibrahimadam193@gmail.com>
2019-03-22 23:51:31 -0400

    Add deps for creating images, add walkthrough
    Changes to be committed:
          modified:   .gitignore
          new file:   examples.py
          new file:   requirements.txt

---
+++ examples.py
@@ -0,0 +1,58 @@
+from f_bidr import read_logical_records
+
+# For creating and viewing an image, and nothing else.
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+
+# Let's start off with MG_4001/F0376_3/FILE_15.
+# I've placed MG_4001/F0376_3 in my repo's root directory as
+# sample-data.
+with open("sample-data/FILE_15", 'rb') as f:
+    contents = f.read()
+
+# Files 12-19 of the magellan data (in any given folder) are a series
+# of one or more "logical records", these unrelated chunks of
+# information. For getting started, that just means one thing: you can
+# choose how much of the file you read in. The largest files are
+# ~100MB and consist of ~5000 logical records (FILE_15 are the
+# largest, always).
+
+# Let's look at two records to start. When you use this on your own,
+# you can leave out the number of records to read to just read them
+# all. A warning: that can take up to 30 seconds.
+records = read_logical_records(contents, 2)
+
+# The actual image data is contained in records['data_block']. It's an
+# array of lines of pixels---with extra information. The extra info
+# describes how many "filler pixels" are at the beginning and end of a
+# given line (slightly more to this story).
+
+# The first line of pixels in the first logical record is:
+records[0]['data_block'][0]['line']
+
+# We can use these lines of pixels to make an image. Let's get just
+# the lines out:
+lines = [x['line'] for x in records[0]['data_block']]
+
+# There's 52 lines of pixels, and 512 pixels in each line. Creating an
+# image (this way) is making an array out of them, and interpreting
+# them as a grayscale image.
+gray = matplotlib.cm.get_cmap('gray')
+image = np.array(lines)
+plt.imsave('sample-image.png', image, cmap=gray)
+
+# Go oogle that. It's so teasingly somewhere between TV static and
+# drab planet surface. You may have questions about whether or not
+# this is correct. I do too. There are helpful references and
+# information both in the logical record, and in the BIDR report which
+# is in papers+documents/MGN-FBIDR-SIS-SDPS-101-RevE.pdf
+# - Page 48 talks about the actual image data. You'll find that there
+#   are two types of images (really four), and their data is
+#   interpreted differently. I don't currently have anything written
+#   to interpret single-look data (but FILE_15 is multi-look data,
+#   which I can work with right now)
+# - Page 50 speaks more about how pixel intensity is derived.
+# - record[0]['secondary_header']['annotation_block']['label'] is some
+#   metadata about the image. Explanations of the contained fields are
+#   on page 39.
