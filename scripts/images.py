8ddab0a1b54ca7a967ef783761cf68a4cf901e6c
adam <ibrahimadam193@gmail.com>
2019-04-04 21:11:30 -0400

    Start on stitching together larger orbits
    Current approach creates one huge picture and inserts slices in there
    one at a time.
    
    Changes to be committed:
          modified:   scripts/images.py

--- scripts/images.py
+++ scripts/images.py
@@ -8,52 +8,63 @@ import numpy as np
 # logical records.
 with open("sample-data/FILE_15", 'rb') as f:
     contents = f.read()
-    #records = read_logical_records(contents, 1000)
+    #records = read_logical_records(contents, 100)
     records = read_logical_records(contents)
 
 gray = matplotlib.cm.get_cmap('gray')
 
-def image_shift(image, shift, total_width, most_negative):
-    # 0 pixel offset means no shifting.
-    # For now we'll say that a positive offset is a right shift
-    # and a negative offset is a left shift.
-    # What's a right shift by i pixels? image[0,0] -> image[0,i]
-    # What gets inserted in the missing pixels? Black.
-    height, width = image.shape
-    picture_left = shift - most_negative
-    black = np.zeros((height, total_width))
-    black[:, picture_left:picture_left + width] = image
-    return black
-
-def image_stitch(records, sort_by, pixel_shift=True):
+# Earlier rows are the top of the image.
+# Earlier columns are the left of the image.
+# pixels earlier in a data row have a smaller pixel offset.
+# pixels later in a data have a later pixel offset.
+# Earlier records have larger line offsets.
+# Later records have smaller line offsets.
+# When concatenating arrays, earlier arrays have lower indexes.
+# So the 1st row of the 1st array is the top row of the image.
+def image_stitch(records, sort_by, save_path=None):
     # The final picture should be wide enough to fit any piece at
     # any of the offsets for those pieces. At minimum, that's a width
     # of the range of offsets (max_offset - min_offset), because a
     # picture's left-most pixel can go anywhere in that range. Then,
     # the picture whose offset is the max will have 512 pixels to the
     # right.
-    #records.sort(key=lambda r: r[sort_by], reverse=True)
     pixel_offsets = [r['reference_offset_pixels'] for r in records]
-    most_neg_offset = min(pixel_offsets)
-    min_offset, max_offset = min(pixel_offsets), max(pixel_offsets)
-    max_width = max_offset - min_offset + 512
-    # Index 0 in the final image will be the most negative offset
-    # pixel.
-    # For now, assume that each picture should directly touch previous
-    # picture.
-    image_data = [[x['line'] for x in r['data']] for r in records]
-    images = [np.array(x) for x in image_data]
-    #shifted_images = []
-    for i in range(len(images)):
-        images[i] = image_shift(
-            images[i], pixel_offsets[i],
-            max_width, most_neg_offset)
-    large_image = np.concatenate(images, axis=0)
-    plt.imsave('/tmp/long-strip.png', large_image, cmap=gray)
-    return large_image
-
-#for i, image in enumerate(images):
-    #plt.imsave(f'sample-images/{i:02}.png', image, cmap=gray)
+    line_offsets = [r['reference_offset_lines'] for r in records]
+    min_pixels, max_pixels = min(pixel_offsets), max(pixel_offsets)
+    min_lines, max_lines = min(line_offsets), max(line_offsets)
+    max_width = max_pixels - min_pixels + 512
+    # I know ahead of time that the last image record should have the
+    # largest line offset.
+    max_height = max_lines - min_lines + records[-1]['line_count']
+    try:
+        master_picture = np.zeros((max_height, max_width), 
+                dtype=np.uint8)
+    except MemoryError as e:
+        print(f'Attempted shape: {max_height}x{max_width}')
+        raise e
+    for record in records:
+        image = np.array([x['line'] for x in record['data']], dtype=np.uint8)
+        pixel_shift = record['reference_offset_pixels']
+        line_shift = record['reference_offset_lines']
+        # The 0th column is the pixel most to the left, thus
+        # min_pixels should map to index 0
+        height, width = image.shape
+        left = pixel_shift - min_pixels
+        # The 1st line in an image becomes the top of the image. Thus
+        # the 1st line of these magellan records is their top. The 1st
+        # (top) line will go in a smaller index, lower lines will go
+        # in higher indexes.
+        # Higher line offsets should be the top of the image. Highest
+        # offset should map to row 0. Lower line offsets should map to
+        # higher indexes. So I negated the shift (so that lower
+        # offsets are now higher numbers) and to make the highest
+        # line offset 0, I added max_lines.
+        top = -line_shift + max_lines
+        master_picture[top:top + height, left:left + width] = image
+    if save_path is None:
+        save_path = 'long-strip.png'
+    plt.imsave(save_path, master_picture, cmap=gray)
+    return master_picture
 
 # Image questions:
 # - What's the orientation of the image lines? I know that the first
