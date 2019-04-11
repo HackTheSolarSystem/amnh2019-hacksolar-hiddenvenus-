09bfa1047db098790761182f13aed84fa532729b
adam <ibrahimadam193@gmail.com>
2019-04-11 01:46:13 -0400

    Make f_bidr_data portable
    It now figures out the path for local data based on project root.
    
    Changes to be committed:
          modified:   f_bidr_data.py

--- f_bidr_data.py
+++ f_bidr_data.py
@@ -1,9 +1,11 @@
 import urllib.request
+import urllib.error
 import os.path
+import os
 import shutil
 
 # I don't wanna think about the version. If I request an orbit, I
-# should get the later version of the orbit, unless I specify
+# should get the latest version of the orbit, unless I specify
 # otherwise.
 #
 # I don't wanna think about paths or downloads. I want python to
@@ -22,13 +24,14 @@ with open('record-order') as f:
             orbits[number] = [path]
 
 f_bidr_root = "http://pds-geosciences.wustl.edu/mgn/mgn-v-rdrs-5-bidr-full-res-v1/"
-data_root = "/home/adam/projects/amnh2019-hacksolar-hiddenvenus/data/"
+project_root = os.path.dirname(os.path.abspath(__file__))
+data_root = os.path.join(project_root, 'data')
 
 def gen_url(number, version, filename):
     return f'{f_bidr_root}{orbits[number][version - 1]}/{filename.upper()}'
 
 def gen_local_path(number, version, filename):
-    return f'{data_root}{orbits[number][version - 1]}/{filename.upper()}'
+    return os.path.join(data_root, orbits[number][version - 1], filename.upper())
 
 def file_exists(number, version, filename):
     return os.path.exists(gen_local_path(number, version, filename))
@@ -37,7 +40,11 @@ def download_orbit_file(number, version, filename):
     """Fetch particular file from internet mirror."""
     url = gen_url(number, version, filename)
     path = gen_local_path(number, version, filename)
-    local_file, headers = urllib.request.urlretrieve(url)
+    try:
+        local_file, headers = urllib.request.urlretrieve(url)
+    except urllib.error.URLError as e:
+        print("Something likely wrong with internet connection.")
+        raise e
     shutil.move(local_file, path)
 
 def get_orbit_file_path(number, filename, version=None):
@@ -45,6 +52,11 @@ def get_orbit_file_path(number, filename, version=None):
     # Use latest version if none given
     if version is None:
         version = len(orbits[number])
+    local_path = gen_local_path(number, version, filename)
+    orbit_dir = os.path.dirname(local_path)
+    header = None
+    if not os.path.exists(orbit_dir):
+        os.makedirs(orbit_dir)
     if not file_exists(number, version, filename):
         download_orbit_file(number, version, filename)
-    return gen_local_path(number, version, filename)
+    return local_path
