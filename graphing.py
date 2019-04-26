3d406ef338955c6ea2c20f584b03cc6d013f8847
adam <ibrahimadam193@gmail.com>
2019-04-26 12:54:08 -0400

    Add labels to graphs produced by graph
    Changes to be committed:
          modified:   graphing.py

--- graphing.py
+++ graphing.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from f_bidr import *
 from f_bidr_data import get_orbit_file_path as orbit
+import itertools
 
 def get(records, *names):
     outputs = []
@@ -19,9 +20,12 @@ def graph(records, *names, **axargs):
     stuff = get(records, *names)
     if len(names) == 1:
         plt.scatter(range(len(stuff)), stuff)
+        ax = plt.gca()
+        ax.set(xlabel='record #', ylabel=names[0])
     else:
         plt.scatter(stuff[0], stuff[1])
-    ax = plt.gca()
+        ax = plt.gca()
+        ax.set(xlabel=names[0], ylabel=names[1])
     if axargs:
         ax.set(**axargs)
     plt.show()
