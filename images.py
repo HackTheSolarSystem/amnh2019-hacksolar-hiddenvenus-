870429b8a7da712a38379fc12831cd33a72b1b94
adam <ibrahimadam193@gmail.com>
2019-04-28 22:17:06 -0400

    New attempt for pixel offsets
    Didn't change how the images were stitched together, but changed how
    pixel offsets for different orbits were calculated. I forgot to divide
    by 75; I tried to get pixel locations on the sinusoidal projection by
    transforming from lat/lon (of the projection origin) to lines/pixels.
    However, I converted to meters, not pixels, and each pixel is 75m wide.
    So I had to divide by 75. The results are still bad, but I caught a
    mistake at least.
    Changes to be committed:
          modified:   images.py

--- images.py
+++ images.py
@@ -24,16 +24,16 @@ def multiple_orbits(pixel_offsets):
     selected_orbits = [orbit(i, 'file_15') for i in [382, 384]]
     data = [read_logical_records(o, 100) for o in selected_orbits]
     origin_lons = [o[0]['proj_origin_lon'] for o in data]
-    #origin_pixels = [math.pi / 180 * l * 6051.8 for l in origin_lons]
-    #least_pixels = min(origin_pixels)
+    origin_pixels = [math.pi / 180 * (l - 180) * 6051.8e3 / 75 for l in origin_lons]
+    least_pixels = min(origin_pixels)
     #pixel_offsets = [0, 20, 40, 60]
-    #pixel_offsets = [round(p - least_pixels) for p in origin_pixels]
+    pixel_offsets = [round(p - least_pixels) for p in origin_pixels]
     #least_lon = min(origin_lons)
     #pixel_offsets = [round((l - least_lon) / 7.1016e-4) 
                      #for l in origin_lons]
     #pixel_offsets = [0, 100]
     print(origin_lons)
-    #print(pixel_offsets)
+    print(pixel_offsets)
     for image_data, offset in zip(data, pixel_offsets):
         for record in image_data:
             record['reference_offset_pixels'] += offset
