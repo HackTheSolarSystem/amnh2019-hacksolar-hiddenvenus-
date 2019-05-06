5a1d3eddc7d4cdcfe23d5e9e39512991d8bac33c
adam <ibrahimadam193@gmail.com>
2019-05-06 00:47:39 -0400

    Clean stitch explanations, use only valid pixels
    Changes to be committed:
          modified:   images.py

--- images.py
+++ images.py
@@ -2,6 +2,7 @@ from f_bidr import logical_record, count_logical_recs, read_logical_records
 from f_bidr_data import get_orbit_file_path as orbit
 
 import numpy as np
+import numpy.ma as ma
 import imageio
 
 # NOTE: File 15 has more than one logical record. It's a series of
@@ -57,6 +58,12 @@ def process_file(filepath, savepath, slices=3):
         start = end
         count += 1
 
+# TODO: Don't think this works for right-look images. The pointer and
+# offset are relative to the east-most pixel for those.
+def make_mask(offset, pointer, length):
+    return np.array([True]*offset + [False]*(pointer - offset) + 
+            [True]*(length - pointer))
+
 # TODO: Some files, such as MG_4002/F0382_4/FILE_13, start from a low
 # line offset and proceed to a larger one. The final product shows
 # slices that are out of order. The image "top" (it's a slice with
@@ -79,32 +86,69 @@ def process_file(filepath, savepath, slices=3):
 #   - height of last picture, whose 1st row is on the row of smallest
 #     offset.
 def image_stitch(records, sort_by, save_path=None):
-    # The final picture should be wide enough to fit any piece at
-    # any of the offsets for those pieces. At minimum, that's a width
-    # of the range of offsets (max_offset - min_offset), because a
-    # picture's left-most pixel can go anywhere in that range. Then,
-    # the picture whose offset is the max will have 512 pixels to the
-    # right.
     pixel_offsets = [r['reference_offset_pixels'] for r in records]
     line_offsets = [r['reference_offset_lines'] for r in records]
-    min_pixels, max_pixels = min(pixel_offsets), max(pixel_offsets)
-    min_lines, max_lines = min(line_offsets), max(line_offsets)
+    left_most = min(pixel_offsets)
+    right_most = max([r['reference_offset_pixels'] + r['line_length'] for r in records])
+    top_most = max(line_offsets)
+    bottom_most = min([r['reference_offset_lines'] - r['line_count'] for r in records])
+    min_pixels = left_most
+    max_lines = top_most
+    #min_pixels, max_pixels = min(pixel_offsets), max(pixel_offsets)
+    #min_lines, max_lines = min(line_offsets), max(line_offsets)
+
+    # Width of master picture:
+    # - Lower pixel offsets are west (left in the logical records and
+    #   master picture), higher pixel offsets are east (right).
+    # - The left-most pixel of the master picture is the 1st pixel of
+    #   the west-most logical record. Thus the offset of this pixel is
+    #   the offset of the west-most logical record.
+    # - The right-most pixel of the master picture is the last pixel
+    #   of the logical record whose width + pixel offset is highest.
+    #   For now, I've assumed a constant width of 512 (which has so
+    #   far been true), which makes this operation simpler: the last
+    #   pixel of the east-most logical record
     # TODO: 512 assumes constant width of each image piece. Otherwise
     # I'll have to do something similar for this as I did for the
     # max_height.
-    max_width = max_pixels - min_pixels + 512
+    #max_width = max_pixels - min_pixels + 512
+    max_width = right_most - left_most 
+
+    # Height of master picture:
+    # - The following is true only for file_15s.
+    # - The 1st (top) pixel in the master picture has the highest line
+    #   offset. This is the 1st line of pixels of the top (north) most
+    #   record.
+    # - The last (bottom) row of pixels in the master picture is the
+    #   last line from the record whose line offset + height is
+    #   greatest. Height is not generally constant among records. But
+    #   line offset monotonically decreases for file_15 records, and
+    #   I've yet to see image overlaps. For an image that's not the
+    #   last to have the lowest pixel, the height of the not-last
+    #   image has to reach far enough down to pass down all other
+    #   image records between this not-last record and the last one.
+    #   line offset. So this should be the last image, plus that
+    #   image's height.
     bottom_image = min(records, key=lambda r:r['reference_offset_lines'])
     bottom_image_lines = bottom_image['line_count']
     # I know ahead of time that the last image record should have the
     # largest line offset. I wanna know the size of image that would
     # appear at the highest offset.
-    max_height = max_lines - min_lines + bottom_image_lines
-    del pixel_offsets
-    del line_offsets
+    #max_height = max_lines - min_lines + bottom_image_lines
+    max_height = top_most - bottom_most
+    #max_lines = max_height
     print(f'Attempting shape: {max_height}x{max_width}')
     master_picture = np.zeros((max_height, max_width), dtype=np.uint8)
+    record_num = 0
     for record in records:
-        image = np.array([x['line'] for x in record['data']], dtype=np.uint8)
+        line_length = record['line_length'] - 4
+        masked_lines = [ma.array(x['line'], dtype=np.uint8, 
+                        mask=make_mask(
+                            x['offset_to_first'], x['pointer_to_last'],
+                            line_length))
+                        for x in record['data']]
+        image = ma.stack(masked_lines, axis=0)
+        #image = np.array([x['line'] for x in record['data']], dtype=np.uint8)
         pixel_shift = record['reference_offset_pixels']
         line_shift = record['reference_offset_lines']
         # The 0th column is the pixel most to the left, thus
@@ -121,7 +165,10 @@ def image_stitch(records, sort_by, save_path=None):
         # offsets are now higher numbers) and to make the highest
         # line offset 0, I added max_lines.
         top = -line_shift + max_lines
-        master_picture[top:top + height, left:left + width] = image
+        region = master_picture[top:top + height, left:left + width]
+        valids = ~image.mask
+        region[:, :][valids] = image[valids]
+        record_num += 1
     return master_picture
 
 # Image questions:
@@ -182,3 +229,6 @@ def image_stitch(records, sort_by, save_path=None):
 # look-up table. It seems that the multi-look images are important.
 # They're created through single look images. So perhaps we don't have
 # to pay attention to single-looks, if we find them.
+
+#if __name__ == "__main__":
+    #process_file(orbit(479, 'file_15'), 'time.png', 8)
